eb6c2f8573c6b8e2c04bb6d6e2a07f333faff02f
Ricardo <ricardoharanopinto@gmail.com>
2024-08-23 19:37:41 -0300

    metodo get no mongoDB

--- src/app.js
+++ src/app.js
@@ -15,28 +15,13 @@ conexao.once("open", () => {
 const app = express()
 app.use(express.json())
 
-const livros = [
-    {
-        id: 1,
-        titulo: "Senhor dos Anéis"
-    },
-    {
-        id: 2,
-        titulo: "Revolucao dos bichos"
-    }
-]
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id)
-    })
-}
 
 app.get('/', (req,res) => {
     res.status(200).send("livraria")
 })
 
-app.get('/livros', (req,res) => {
+app.get('/livros', async (req,res) => {
+    const listaLivros = await livro.find({});
     res.status(200).json(livros);
 })
 
